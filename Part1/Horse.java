6770024b2dea6d2f6754a780ddfa010749d127fe
Lee En Yee <yanleeenyee@gmail.com>
2025-03-17 13:29:27 +0000

    Initial commit

---
+++ Part1/Horse.java
@@ -0,0 +1,78 @@
+
+/**
+ * Write a description of class Horse here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Horse
+{
+    //Fields of class Horse
+    char horseSymbol;
+    String horseName;
+    double horseConfidence;
+    
+
+    //Constructor of class Horse
+    /**
+     * Constructor for objects of class Horse
+     */
+    public Horse(char horseSymbol, String horseName, double horseConfidence)
+    {
+       
+    }
+    
+    
+    
+    //Other methods of class Horse
+    public void fall()
+    {
+        
+    }
+    
+    public double getConfidence()
+    {
+        return horseConfidence;
+    }
+    
+    public int getDistanceTravelled()
+    {
+        
+    }
+    
+    public String getName()
+    {
+        return this.horseName;
+    }
+    
+    public char getSymbol()
+    {
+        return this.horseSymbol;
+    }
+    
+    public void goBackToStart()
+    {
+        
+    }
+    
+    public boolean hasFallen()
+    {
+        
+    }
+
+    public void moveForward()
+    {
+        
+    }
+
+    public void setConfidence(double newConfidence)
+    {
+        this.horseConfidence = newConfidence;
+    }
+    
+    public void setSymbol(char newSymbol)
+    {
+        this.horseSymbol = newSymbol;
+    }
+    
+}
