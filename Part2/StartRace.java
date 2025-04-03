f65ee6351a77406b196cf761a19bded6eba4a2ea
Lee En Yee <yanleeenyee@gmail.com>
2025-04-03 21:56:30 +0100

    Fixed button layout

--- Part2/StartRace.java
+++ Part2/StartRace.java
@@ -4,20 +4,23 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
 public class StartRace extends JPanel{
 
     private HorseRaceGUI mainGUI;
-
+    
     StartRace(HorseRaceGUI mainGUI){
 
         this.mainGUI = mainGUI;
-
+    
         setLayout(new BorderLayout());
 
+        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));   //flowlayout assigns left by default
         JButton backButton = new JButton("Back to Menu");
-        backButton.addActionListener(e -> mainGUI.showStartMenu());
-        add(backButton, BorderLayout.SOUTH);
 
+        backButton.addActionListener(e -> mainGUI.showStartMenu());
+        bottomPanel.add(backButton);
+        add(bottomPanel, BorderLayout.SOUTH);              
     }
 }
