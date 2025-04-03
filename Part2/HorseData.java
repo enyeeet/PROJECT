9bb6646c0a22af6ec8d595e00899e8d2cbbe4f21
Lee En Yee <yanleeenyee@gmail.com>
2025-04-03 18:40:47 +0100

    Different breeds initialise with different stats, new setter methods, new method finaliseHorseStats() that applies track condition and shape effects to horse stats after horse customisation as well as the respective methods needed for that

--- Part2/HorseData.java
+++ Part2/HorseData.java
@@ -11,6 +11,13 @@ public class HorseData {
     private String bridle;
     private String hat;
 
+    private double horseConfidence;
+    private double horseSpeed;
+    private double horseStamina;
+
+    private String trackCondition;
+    private String trackShape;
+
     HorseData(String name, String breed, String coatColor, String symbol, String saddleDesign, String saddleColor,
                      String horseShoes, String bridle, String hat) {
         this.name = name;
@@ -22,6 +29,62 @@ public class HorseData {
         this.horseShoes = horseShoes;
         this.bridle = bridle;
         this.hat = hat;
+
+        switch (this.breed) {
+            case "Thoroughbred" -> {
+                this.horseSpeed = 0.6;
+                this.horseStamina = 0.7;
+                this.horseConfidence = 0.8;
+            }
+            case "Arabian" -> {
+                this.horseSpeed = 0.8;
+                this.horseStamina = 0.5;
+                this.horseConfidence = 0.7;
+            }
+            case "Quarter Horse" -> {
+                this.horseSpeed = 0.6;
+                this.horseStamina = 0.6;
+                this.horseConfidence = 0.5;
+            }
+        }
+    }
+
+    public void finaliseHorseStats(){
+        trackConditionEffect();
+        trackShapeEffect();
+    }
+
+    public void trackConditionEffect(){
+        switch(trackCondition){
+            case "Muddy":
+                this.horseSpeed -= 0.2;
+                this.horseStamina -= 0.2;
+                break;
+            case "Icy":
+                this.horseConfidence -= 0.3;
+                break;
+            case "Dry":
+                this.horseSpeed += 0.2;
+                this.horseConfidence += 0.2;
+                break;
+        }
+    }
+
+    public void trackShapeEffect(){
+        if(trackShape.equals("Oval")){
+            this.horseSpeed += 0.1;
+        }
+        else if(trackShape.equals("Figure Eight")){
+            this.horseSpeed += 0.2;
+        }
+    }
+
+    public void setTrackCondition(String trackCondition) {
+        this.trackCondition = trackCondition;
+    }
+
+    public void setTrackShape(String trackShape) {
+        this.trackShape = trackShape;
     }
 
     // Getters
@@ -34,4 +97,8 @@ public class HorseData {
     public String getHorseShoes() { return horseShoes; }
     public String getBridle() { return bridle; }
     public String getHat() { return hat; }
+    public double getHorseConfidence() { return horseConfidence; }
+    public double getHorseSpeed() { return horseSpeed; }
+    public double getHorseStamina() { return horseStamina; }
+
 }
