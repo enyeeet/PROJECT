913b41707963a59f3aa75e30a85fc8c9457ff23c
Lee En Yee <yanleeenyee@gmail.com>
2025-04-05 12:01:43 +0100

    new class for tracking and controlling the movement of the horse during the race, which is just a modified version of class Horse from part1, might combine this horsedata depending on the complexity, and i have yet to remove the unused methods

---
+++ Part2/InRaceHorse.java
@@ -0,0 +1,118 @@
+package Part2;
+
+/**
+ * Write a description of class Horse here.
+ *
+ * @author Lee En Yee
+ * @version 1
+ */
+public class InRaceHorse
+{
+    //Fields of class Horse
+    private String horseSymbol;
+    private String horseName;
+    private double horseConfidence;
+    private boolean fallen;
+    private int distanceTravelled;
+
+
+    //Constructor of class Horse
+    /**
+     * Constructor for objects of class Horse
+     */
+    public InRaceHorse(String horseSymbol, String horseName, double horseConfidence)
+    {
+        this.horseSymbol = horseSymbol;
+        this.horseName = horseName;
+        setConfidence(horseConfidence);
+        this.fallen = false;
+        this.distanceTravelled = 0;
+    }
+
+    //Other methods of class Horse
+    public void fall()
+    {
+        this.fallen = true;
+        decreaseConfidence(0.02);
+    }
+
+    public double getConfidence()
+    {
+        return this.horseConfidence;
+    }
+
+    public int getDistanceTravelled()
+    {
+        return this.distanceTravelled;
+    }
+
+    public String getName()
+    {
+        return this.horseName;
+    }
+
+    public String getSymbol()
+    {
+        return this.horseSymbol;
+    }
+
+    public void goBackToStart()
+    {
+        this.fallen = false;
+        this.distanceTravelled = 0;
+    }
+
+    public boolean hasFallen()
+    {
+        return this.fallen;
+    }
+
+    public void moveForward()
+    {
+        if(!this.fallen){
+            this.distanceTravelled++;
+        }
+    }
+
+    public void setConfidence(double newConfidence)
+    {
+        if(newConfidence < 0.0 || newConfidence > 1.0){
+            System.out.println("Confidence rating must be between 0.0 and 1.0.");
+        }
+
+        this.horseConfidence = newConfidence;
+    }
+
+    public void setSymbol(String newSymbol)
+    {
+        this.horseSymbol = newSymbol;
+    }
+
+    public void increaseConfidence(double amount) {
+        if (this.horseConfidence + amount > 1.0) {
+            this.horseConfidence = 1.0;
+            System.out.println(this.getName() + " already has maximum confidence.");
+        }
+        else {
+            this.horseConfidence += amount;
+        }
+        this.horseConfidence = roundUpToTwoDP(this.horseConfidence);
+    }
+
+    // Decrease confidence after falling
+    public void decreaseConfidence(double amount) {
+        if (this.horseConfidence - amount < 0.0) {
+            this.horseConfidence = 0.0;
+            System.out.println(this.getName() + " already has no confidence....");
+        }
+        else {
+            this.horseConfidence -= amount;
+        }
+        this.horseConfidence = roundUpToTwoDP(this.horseConfidence);
+    }
+
+    private double roundUpToTwoDP(double value) {
+        return Math.round(value * 100.0) / 100.0;
+    }
+}
+
