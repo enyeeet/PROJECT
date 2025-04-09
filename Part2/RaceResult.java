9c00621d9357e7413107e6bcf0eab25e18200d41
Lee En Yee <yanleeenyee@gmail.com>
2025-04-09 17:06:04 +0100

    added new didNotFinish variable to track horses that fell

--- Part2/RaceResult.java
+++ Part2/RaceResult.java
@@ -6,22 +6,24 @@ public class RaceResult {
     private double avgSpeed;
     private double initialConfidence;
     private boolean isWinner;
+    private boolean didNotFinish;
     private String trackShape;
     private String trackCondition;
 
-    public RaceResult(double finishTime, double speed, double initialConfidence, boolean isWinner, String trackShape, String trackCondition) {
+    public RaceResult(double finishTime, double speed, double initialConfidence, boolean isWinner, String trackShape, String trackCondition, boolean didNotFinish) {
         this.finishTime = finishTime;
         this.avgSpeed = speed;
         this.initialConfidence = initialConfidence;
         this.isWinner = isWinner;
         this.trackShape = trackShape;
         this.trackCondition = trackCondition;
+        this.didNotFinish = didNotFinish;
     }
 
     public double getFinishTime() { return finishTime; }
     public double getInitialConfidence() { return initialConfidence; }
     public boolean isWinner() { return isWinner; }
+    public boolean didNotFinish() { return didNotFinish; }
     public String getTrackShape() { return trackShape; }
     public String getTrackCondition() { return trackCondition; }
 }
-
